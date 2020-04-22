2f00e521c3618c4f0beaed9d9954faad21fffaf5
dhruvix <dhruvix7@gmail.com>
2020-04-22 22:31:57 +0530

    Updates

---
+++ precache-manifest.956c76885b69c6edc7c1d59082827be0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e0fed9a12ab44d6f80ccd94cf5cadd8",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "b47614ef0c2edec53743",
+    "url": "/react-deploy/static/css/main.37af9139.chunk.css"
+  },
+  {
+    "revision": "30d0eda4c6200fc3652c",
+    "url": "/react-deploy/static/js/2.ba466430.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.ba466430.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b47614ef0c2edec53743",
+    "url": "/react-deploy/static/js/main.42df4463.chunk.js"
+  },
+  {
+    "revision": "ab96405cd50425e31642",
+    "url": "/react-deploy/static/js/runtime-main.2434a08a.js"
+  }
+]);
