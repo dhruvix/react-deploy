69a405aa3b37ab4173a05e48512ba8d0bd1b58a7
dhruvix <dhruvix7@gmail.com>
2020-06-25 12:24:28 +0530

    updated after deploying to gh pages

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  function redirect(){
+    alert("the port number of this site is:",window.location.port);
+    alert("Most browsers will not display default port numbers (80 for http and 443 for https)")
+  }
+
   return (
     <div className="App">
-      <h1>Get Schwifty !</h1>
+      <h1 onClick={redirect} >Get Schwifty !</h1>
     </div>
   );
 }
